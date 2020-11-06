2f2ab81cddfc72bb11b2be8da64fa005d7b44147
Athanasios Xygkis <athanasios.xygkis@epfl.ch>
2020-11-06 22:43:09 +0100

    Split gnome tools into tools and themes

--- install_gnome_tools.sh
+++ install_gnome_tools.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
 yay -S gnome-autoar gnome-desktop gnome-getting-started-docs \
-    gnome-icon-theme gnome-icon-theme-symbolic gnome-keyring \
-    gnome-screenshot gnome-system-monitor
-
-yay -S adwaita-icon-theme arc-gtk-theme arc-icon-theme \
-    gnome-icon-theme gnome-icon-theme-symbolic gnome-themes-extra \
-    hicolor-icon-theme kiconthemes paper-gtk-theme-git \
-    sound-theme-freedesktop vertex-themes-git
+    gnome-keyring gnome-screenshot gnome-system-monitor
---
+++ install_themes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+yay -S gnome-icon-theme gnome-icom-themes-extra gnome-icon-theme-symbolic
+
+# yay -S adwaita-icon-theme arc-gtk-theme arc-icon-theme \
+#    hicolor-icon-theme sound-theme-freedesktop
+
+yay -S vertex-icons-git vertex-themes-git paper-gtk-theme-git
+
+
