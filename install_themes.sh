3bd03980fabd728caaad60c2d0256d99886600c4
Athanasios Xygkis <saggy00@gmail.com>
2020-11-07 00:16:05 +0100

    Add icons theme

--- install_themes.sh
+++ install_themes.sh
@@ -7,4 +7,4 @@ yay -S gnome-icon-theme gnome-icom-themes-extra gnome-icon-theme-symbolic
 # yay -S adwaita-icon-theme arc-gtk-theme arc-icon-theme \
 #    hicolor-icon-theme sound-theme-freedesktop
 
-yay -S vertex-icons-git vertex-themes-git paper-gtk-theme-git
+yay -S vertex-icons-git vertex-themes-git paper-gtk-theme-git paper-icon-theme-git
