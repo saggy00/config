80c1f5670b477e151ad755949ec55afd230de706
Athanasios Xygkis <athanasios.xygkis@epfl.ch>
2020-11-06 21:51:37 +0100

    Add repo fonts

---
+++ install_repo_fonts.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+yay -S adobe-source-code-pro-fonts cantarell-fonts font-bh-ttf \
+    gnu-free-fonts gsfonts noto-fonts noto-fonts-extra \
+    powerline-fonts terminus-font ttf-qurancomplex-fonts \
+    ttf-ubuntu-font-family \
+    xorg-fonts-100dpi xorg-fonts-75dpi xorg-fonts-alias-100dpi \
+    xorg-fonts-alias-75dpi xorg-fonts-alias-misc \
+    xorg-fonts-encodings xorg-fonts-misc xorg-xlsfonts
