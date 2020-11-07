998e6aaf0302c6aecf7201585225dac937c7e9d4
Athanasios Xygkis <athanasios.xygkis@epfl.ch>
2020-11-07 14:17:04 +0100

    Add executable .xsession

--- lightdm/install.sh
+++ lightdm/install.sh
@@ -6,5 +6,10 @@ yay -S accountsservice lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
 sudo cp "$DIR"/login_bg.png /etc/lightdm
 sudo lightdm-gtk-greeter-settings
 sudo systemctl enable lightdm.service
+
 rm ~/.xinitrc
+
+sed -i '1s/^/#!\/bin\/bash\n/' ~/.xsession
+chmod u+x ~/.xsession
+
 echo "Reboot"
