7c1a3fd0363c947d3e924da6bd101bd6cc578ec0
Athanasios Xygkis <athanasios.xygkis@epfl.ch>
2020-11-07 14:04:44 +0100

    Fix lightdm

--- lightdm/install.sh
+++ lightdm/install.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-yay -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
+yay -S accountsservice lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
 sudo cp "$DIR"/login_bg.png /etc/lightdm
 sudo lightdm-gtk-greeter-settings
 sudo systemctl enable lightdm.service
