c80822262d9b8e1f8a7dfec00bdf89cf7dcc6a90
Athanasios Xygkis <athanasios.xygkis@epfl.ch>
2020-11-07 12:44:29 +0100

    lightdm fix settings editor

--- lightdm/install.sh
+++ lightdm/install.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 yay -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
 sudo cp "$DIR"/login_bg.png /etc/lightdm
-sudo lightdm-greeter-settings
+sudo lightdm-gtk-greeter-settings
 sudo systemctl enable lightdm.service
 rm ~/.xinitrc
 echo "Reboot"
