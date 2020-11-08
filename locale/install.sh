5037f9cc51758958cab5638ab36d5a3d9f73a06c
Athanasios Xygkis <athanasios.xygkis@epfl.ch>
2020-11-08 19:55:49 +0100

    Locale

---
+++ locale/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+sudo hwclock --systohc
+
+sudo cp "$DIR"/hostname /etc/hostname
+sudo cp "$DIR"/hosts /etc/hosts
+
+sed -i '/fr_CH.UTF-8 UTF-8/s/^#//' /etc/locale.gen
+locale-gen
+sudo cp "$DIR"/locale.conf /etc/locale.conf
