a87fa0f63aa1d30c19844f180ede9cc262ecbce1
Athanasios Xygkis <athanasios.xygkis@epfl.ch>
2020-11-08 18:53:15 +0100

    Remove keychain and add gpg

--- ssh/install.sh
+++ ssh/install.sh
@@ -2,5 +2,6 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-yay -S openssh keychain
-echo "keychain" >> ~/.xsession
+yay -S openssh gnupg
+echo 'eval $(ssh-agent)' >> ~/.xsession
+echo 'eval $(gpg-agent)' >> ~/.xsession
