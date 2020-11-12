28d1b8f2c221eb946161f9565253f605659fb652
Athanasios Xygkis <athanasios.xygkis@epfl.ch>
2020-11-12 10:45:31 +0100

    Mention trusting our gpg keys

--- ssh/install.sh
+++ ssh/install.sh
@@ -5,3 +5,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 yay -S openssh gnupg
 echo 'eval $(ssh-agent)' >> ~/.xsession
 echo 'eval $(gpg-agent)' >> ~/.xsession
+
+echo 'When importing your private keys, do not forget to trust them ultimately!'
+echo 'Run `gpg --edit-key <email>`, then type `trust` and follow the instructions'
