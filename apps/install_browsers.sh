94333ef085b4fbbe24ae9ec35a915380526afc14
Athanasios Xygkis <athanasios.xygkis@epfl.ch>
2020-11-12 09:59:52 +0100

    Install gvfs with nautilus

--- apps/install_browsers.sh
+++ apps/install_browsers.sh
@@ -4,5 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 yay -S nautilus gedit firefox file-roller
 
+yay -S gvfs gvfs-nfs gvfs-smb gvfs-mtp
+
 yay -S xdg-user-dirs xdg-utils
 xdg-user-dirs-update
